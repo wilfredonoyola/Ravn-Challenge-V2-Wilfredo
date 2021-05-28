b36a06188936785b663fc3e8094183bbc1bafd85
wilfredonoyola <wilfredon163@gmail.com>
2021-05-28 09:38:22 -0600

    Initial package and initial basic app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PeopleComponent } from './people/people.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
