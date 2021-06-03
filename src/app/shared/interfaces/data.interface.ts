eb2321474d8b2519a3bd653391bd38af989485c4
wilfredonoyola <wilfredon163@gmail.com>
2021-06-03 09:27:42 -0600

    Added methods to get data from graphql

--- src/app/shared/interfaces/data.interface.ts
+++ src/app/shared/interfaces/data.interface.ts
@@ -1,23 +1,48 @@
 export interface APIResponse<T> {
-    people: T;
+  allPeople: T
 }
 
-export interface DataResponse{
-    allPeople: APIResponse<People[]>;
+export interface DataResponse {
+  people: APIResponse<People[]>;
+  pageInfo: APIResponse<Pagination>
 }
 
-
-export interface HomeWorld{
-    name: string;
+export interface HomeWorld {
+  name: string
 }
 
-export interface Species{
-    name: string;
+export interface Species {
+  name: string
 }
 
-export interface People{
-    id: number;
-    name: string;
-    homeworld: HomeWorld;
-    specie: Species;
+export interface People {
+  id: number
+  name: string
+  homeworld: HomeWorld
+  species: Species
+  eyeColor:String
+  hairColor:String
+  skinColor:String
+  birthYear:String
+  vehicleConnection: VehicleConnection
+}
+
+export interface VehicleConnection{
+  vehicles: Vehicle[]
+}
+
+export interface Vehicle{
+  name: string;
+}
+
+export interface Pagination{
+  startCursor: string;
+  endCursor: string;
+  hasNextPage: boolean;
+  hasPreviousPage: boolean;
+}
+
+export interface ListPeople{
+  people: People[],
+  pageInfo: Pagination
 }
--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,46 +1,100 @@
-import { query } from '@angular/animations';
-import { Injectable } from '@angular/core';
-import { Apollo, gql } from 'apollo-angular';
-import { BehaviorSubject } from 'rxjs';
-import { take, tap } from 'rxjs/operators';
-import { DataResponse, People } from '../interfaces/data.interface';
-
-const QUERY = gql`
-{
-  allPeople {
-    people {
-      id
-      name      
-      homeworld{
-        name
-      }
-      species{
-        name
-      }
-    }
-  }
-}
- `;
+import { Injectable } from '@angular/core'
+import { Apollo, gql } from 'apollo-angular'
+import { DocumentNode } from 'graphql'
+import { Observable, of } from 'rxjs'
+import { catchError, pluck, take, tap } from 'rxjs/operators'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
-  private peopleSubject = new BehaviorSubject<People[]>([]);
-  people$ = this.peopleSubject.asObservable();
+  constructor(private apollo: Apollo) {}
 
-  constructor(private apollo: Apollo) { 
-    this.getDataAPI();
+  /**
+   * Return a new query
+   * @param first - number of results per page
+   * @param after - optional value, pointer to get other results
+   * @returns 
+   */
+  private getQuerie(first: number, after?: string): DocumentNode {
+    const _after = after ? after : ''
+    const query = gql`
+    {
+      allPeople(first: ${first}, after: "${_after}") {
+        pageInfo{
+          startCursor
+          endCursor
+          hasNextPage
+          hasPreviousPage
+        }
+        people {
+          id
+          name
+          eyeColor
+          hairColor
+          skinColor
+          birthYear      
+          homeworld{
+            name
+          }
+          species{
+            name
+          }
+
+          vehicleConnection{
+            vehicles{
+              name   
+            }
+          }
+        }
+      }
+    }
+    `
+    return query
   }
   
-  private getDataAPI(): void{
-    this.apollo.watchQuery<DataResponse>({
-      query: QUERY
-    }).valueChanges.pipe(
-      take(1),  
-      tap(({data}) => {
-      const {allPeople} = data;
-      this.peopleSubject.next(allPeople.people);
-    })).subscribe();
+  /**
+   * Get people by pagination
+   * @param first - number of results per page
+   * @param after - optional value, pointer to get other results
+   * @returns 
+   */
+  getPeopleByPage(first: number, after?: string): Observable<any> {
+    const QUERY = this.getQuerie(first, after)
+
+    return this.apollo
+      .watchQuery<any>({
+        query: QUERY,
+      })
+      .valueChanges.pipe(
+        take(1),
+        pluck('data', 'allPeople'),
+        tap(() => {}),
+        catchError(this.handleError<any[]>('getPeople', []))
+      )
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      // console.error(error) // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`)
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T)
+    }
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    console.log(`dataService: ${message}`)
   }
 }
